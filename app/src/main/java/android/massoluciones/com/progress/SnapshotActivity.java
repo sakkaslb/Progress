015d9bc678ec898a0f36e4839b75a7d64800086c
Stefanie Lucero <slucero36@gmail.com>
2015-10-22 23:12:57 -0500

    Dos fotos

--- app/src/main/java/android/massoluciones/com/progress/SnapshotActivity.java
+++ app/src/main/java/android/massoluciones/com/progress/SnapshotActivity.java
@@ -3,7 +3,9 @@ package android.massoluciones.com.progress;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,27 +13,33 @@ import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by Stefanie on 17/10/2015.
  */
-public class SnapshotActivity extends Activity {
+public class SnapshotActivity extends Activity implements View.OnClickListener{
 
-    ImageView img;
+    ImageView img, imgpicked;
     private Uri mUri;
     Bitmap mPhoto;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snapshot);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         img=(ImageView) findViewById(R.id.imgSnapshot);
+        imgpicked=(ImageView) findViewById(R.id.imgPicked);
+        imgpicked.setOnClickListener(this);
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         File pdfFolder = new File(Environment.getExternalStorageDirectory()+"/progressapp");
         if (!pdfFolder.exists()) {
@@ -42,6 +50,7 @@ public class SnapshotActivity extends Activity {
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
         mUri = Uri.fromFile(f);
         startActivityForResult(intent,0);
+
     }
 
     @Override
@@ -58,11 +67,27 @@ public class SnapshotActivity extends Activity {
                         mPhoto = MediaStore.Images.Media.getBitmap(cr, mUri);
                         img.setImageBitmap(mPhoto);
 
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
 
                 }
+                break;
+            }
+            case 1:{
+                if (resultCode==Activity.RESULT_OK){
+                    Uri selectedImage = data.getData();
+                    InputStream imageStream = null;
+                    try {
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+                    imgpicked.setImageBitmap(yourSelectedImage);
+                }
+
                 break;
             }
         }
@@ -76,5 +101,18 @@ public class SnapshotActivity extends Activity {
                 NavUtils.navigateUpFromSameTask(this);
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.imgPicked:{
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, ""), 1);
+                break;
+            }
+        }
     }
 }
