05c34a2f5d35a49d691dcccf2d1e42765f55713f
Stefanie Lucero <slucero36@gmail.com>
2015-12-01 23:11:45 -0500

    Social tools casi lista

--- app/src/main/java/android/massoluciones/com/progress/SocialActivity.java
+++ app/src/main/java/android/massoluciones/com/progress/SocialActivity.java
@@ -1,19 +1,68 @@
 package android.massoluciones.com.progress;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-/**
- * Created by Stefanie on 17/10/2015.
- */
 public class SocialActivity extends Activity{
+    Button btnFacebook, btnTwitter, btnInstagram;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        btnFacebook=(Button) findViewById(R.id.btnSocialFacebook);
+        btnTwitter=(Button) findViewById(R.id.btnSocialTwitter);
+        btnInstagram=(Button) findViewById(R.id.btnSocialInstagram);
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/1623101651285178"));
+                    startActivity(intent);
+                } catch(Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.facebook.com/keeprogress")));
+                }
+            }
+        });
+        btnInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://instagram.com/_u/keeprogress");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.instagram.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://instagram.com/keeprogress")));
+                }
+            }
+        });
+        btnTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("twitter://user?user_id=keeprogress_");
+                Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+                likeIng.setPackage("com.twitter.android");
+
+                try {
+                    startActivity(likeIng);
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://twitter.com/keeprogress_")));
+                }
+            }
+        });
     }
 
     @Override
