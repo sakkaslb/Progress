c098afb217f6a56d89b286e7ca68a3aef9717dec
Stefanie Lucero <slucero36@gmail.com>
2015-12-13 18:38:47 -0500

    Monetizing app

--- app/src/main/java/progress/massoluciones/com/progress/WeightActivity.java
+++ app/src/main/java/progress/massoluciones/com/progress/WeightActivity.java
@@ -13,8 +13,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
+
 
 public class WeightActivity extends Activity {
+    PublisherInterstitialAd mPublisherInterstitialAd;
     EditText txtWeight;
     TextView cmbMeasure;
     Button btnWeight;
@@ -24,6 +29,18 @@ public class WeightActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weightcheckin);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mPublisherInterstitialAd = new PublisherInterstitialAd(this);
+       // mPublisherInterstitialAd.setAdUnitId("/6499/example/interstitial");
+        mPublisherInterstitialAd.setAdUnitId("ca-app-pub-5301173135650038/2584179900");
+
+        mPublisherInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                registrarPeso();
+            }
+        });
+        requestNewInterstitial();
         txtWeight=(EditText) findViewById(R.id.txtWeight);
         cmbMeasure=(TextView) findViewById(R.id.txtMeasure);
         btnWeight=(Button)findViewById(R.id.btnWeightCheck);
@@ -33,25 +50,38 @@ public class WeightActivity extends Activity {
         btnWeight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    weight=Integer.parseInt(txtWeight.getText().toString());
-                    if (weight!=null && weight>0 &&weight<600){
-                        Intent vintent=new Intent().setClass(WeightActivity.this,SnapshotActivity.class);
-                        Bundle b=new Bundle();
-                        b.putInt("peso",weight);
-                        vintent.putExtras(b);
-                        startActivity(vintent);
-                        finish();
-                    } else {
-
-                        Toast.makeText(WeightActivity.this,getResources().getString(R.string.weight_validation),Toast.LENGTH_LONG).show();
-                    }
-                }catch (Exception ex){
-                    Toast.makeText(WeightActivity.this,getResources().getString(R.string.weight_validation),Toast.LENGTH_LONG).show();
+                if (mPublisherInterstitialAd.isLoaded()) {
+                    mPublisherInterstitialAd.show();
+                } else {
+                   registrarPeso();
                 }
-
 
             }
         });
+    }
+    private void requestNewInterstitial() {
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder()
+               /* .addTestDevice("F14C3CFE12CD91C9C9AA1F37B87D2043")*/
+                .build();
+
+        mPublisherInterstitialAd.loadAd(adRequest);
+    }
+    private void registrarPeso(){
+        try {
+            weight = Integer.parseInt(txtWeight.getText().toString());
+            if (weight != null && weight > 0 && weight < 600) {
+                Intent vintent = new Intent().setClass(WeightActivity.this, SnapshotActivity.class);
+                Bundle b = new Bundle();
+                b.putInt("peso", weight);
+                vintent.putExtras(b);
+                startActivity(vintent);
+                finish();
+            } else {
+
+                Toast.makeText(WeightActivity.this, getResources().getString(R.string.weight_validation), Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception ex) {
+            Toast.makeText(WeightActivity.this, getResources().getString(R.string.weight_validation), Toast.LENGTH_LONG).show();
+        }
     }
 }
